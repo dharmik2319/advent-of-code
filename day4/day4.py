2b0c9752d57e84895326aba0890507a76783d391
Dharmik Kaku <126943942+dharmik2319@users.noreply.github.com>
2023-11-16 15:14:34 +0530

    day 4 2022

---
+++ day4/day4.py
@@ -0,0 +1,25 @@
+numOfOverlap = 0
+
+with open('day4.txt','r') as file:
+    lines = file.read().splitlines()
+
+lines = [el.split(',') for el in lines]
+lines = [[tuple(i.split('-')) for i in el] for el in lines]
+lines = [[[int(i) for i in el] for el in line] for line in lines]
+lines = [[tuple(i) for i in el] for el in lines]
+
+for line in lines:
+    if set(range(line[0][0],line[0][1]+1)).issubset(range(line[1][0],line[1][1]+1)) or set(range(line[1][0],line[1][1]+1)).issubset(range(line[0][0],line[0][1]+1)):
+        numOfOverlap+=1
+        pass
+    else:
+        pass
+
+part2overlap = 0
+
+for line in lines:
+    if len(set(range(line[0][0],line[0][1]+1)).intersection(set(range(line[1][0],line[1][1]+1)))) > 0:
+        part2overlap+=1
+        pass
+    else:
+        pass
